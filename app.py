17a3a0fd0044b3fcdcb7238e82f19e9a5337f4cc
21A91A05I1 <97424614+21A91A05I1@users.noreply.github.com>
2025-03-18 19:59:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template
+import os
+import subprocess
+import datetime
+import pytz
+
+app = Flask(__name__)
+
+@app.route('/htop')
+def htop():
+    username = os.getlogin()
+
+    ist = pytz.timezone('Asia/Kolkata')
+    now_ist = datetime.datetime.now(ist)
+
+    top_output = subprocess.check_output(['top','-bn1']).decode()
+
+    full_name = "Rayudu Ramya Sri"
+
+    return render_template('index.html',full_name=full_name,username=username,now_ist=now_ist,top_output=top_output)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000,debug=True)
